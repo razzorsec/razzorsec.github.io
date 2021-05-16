48d855f8510f4c184fabfc3a5846c0efbe7ef72c
razzorsec <54918791+razzorsec@users.noreply.github.com>
2021-05-16 21:18:41 +0530

    Update scriptForHelpline.js

--- covidcare/scriptForHelpline.js
+++ covidcare/scriptForHelpline.js
@@ -5,9 +5,9 @@ xmlhttp.onreadystatechange = function () {
 
     let i;
     for (i = 0; i < data.length; i++) {
-      let info = data[i]["gsx$_d180g"]["$t"];
-      let stateName = data[i]["gsx$_d2mkx"]["$t"];
-      let helpNumber = data[i]["gsx$_cssly"]["$t"];
+      let info = data[i]["gsx$_cn6ca"]["$t"];
+      let stateName = data[i]["gsx$_cokwr"]["$t"];
+      let helpNumber = data[i]["gsx$_cpzh4"]["$t"];
       
 
 
@@ -30,7 +30,7 @@ xmlhttp.onreadystatechange = function () {
 
 xmlhttp.open(
   "GET",
-  "https://spreadsheets.google.com/feeds/list/18cCyFbxG6AvhNsk5JLIFCwulcwHvmzxu1kI6decYSNw/od6/public/values?alt=json",
+  "https://spreadsheets.google.com/feeds/list/18cCyFbxG6AvhNsk5JLIFCwulcwHvmzxu1kI6decYSNw/2/public/values?alt=json",
   true
 );
 xmlhttp.send();
