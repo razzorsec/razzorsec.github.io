d91612215557e9c730b05c4ec4883f8054fd5240
razzorsec <54918791+razzorsec@users.noreply.github.com>
2022-07-30 00:05:07 +0530

    migrated to v4

--- covidcare/script.js
+++ covidcare/script.js
@@ -1,16 +1,16 @@
 let xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function () {
   if (this.readyState == 4 && this.status == 200) {
-    let data = JSON.parse(this.responseText).feed.entry;
+    let data = JSON.parse(this.responseText).values;
 
     let i;
-    for (i = 0; i < data.length; i++) {
-      let resc = data[i]["gsx$_cn6ca"]["$t"];
-      let name = data[i]["gsx$_cokwr"]["$t"];
-      let amt = data[i]["gsx$_cpzh4"]["$t"];
-      let city = data[i]["gsx$_cre1l"]["$t"];
-      let contact = data[i]["gsx$_chk2m"]["$t"];
-      let status = data[i]["gsx$_ciyn3"]["$t"];
+    for (i = 1; i < data.length; i++) {
+      let resc = data[i][0];
+      let name = data[i][1];
+      let amt = data[i][2];
+      let city = data[i][3];
+      let contact = data[i][4];
+      let status = data[i][5];
 
 
       document.getElementById("showData").innerHTML +=
@@ -41,7 +41,7 @@ xmlhttp.onreadystatechange = function () {
 
 xmlhttp.open(
   "GET",
-  "https://spreadsheets.google.com/feeds/list/18cCyFbxG6AvhNsk5JLIFCwulcwHvmzxu1kI6decYSNw/od6/public/values?alt=json",
+  "https://sheets.googleapis.com/v4/spreadsheets/18cCyFbxG6AvhNsk5JLIFCwulcwHvmzxu1kI6decYSNw/values/Resources?alt=json&key=AIzaSyDOK2wTjiLGwwJva-C9Y4u-0oVGNzqPUJk",
   true
 );
 xmlhttp.send();
